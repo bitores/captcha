f1c636eb2c093ec1954cef76dd1dbcfed2ef5b8b
bitores <773155801@qq.com>
2020-10-25 19:23:41 +0800

    f

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,14 @@ import less from 'rollup-plugin-less';
 import url from 'rollup-plugin-url'
 import svgr from '@svgr/rollup'
 
+import simplevars from 'postcss-simple-vars';
+import nested from 'postcss-nested';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
+import postcssModules from 'postcss-modules'
+import postcssPresetEnv from 'postcss-preset-env';
+
+import fs from 'fs';
 import pkg from './package.json'
 
 export default {
@@ -24,10 +32,32 @@ export default {
     }
   ],
   plugins: [
+    // less(),
     external(),
     postcss({
-      modules: true
+      // modules: true,
+      plugins: [
+        // simplevars(),
+        // nested(),
+        // postcssPresetEnv(),
+        // cssnano(),
+        // postcssModules({
+        //   getJSON: function (cssFileName, json, outputFileName) {
+        //     var path = require("path");
+        //     var cssName = path.basename(cssFileName, ".css");
+        //     var jsonFileName = path.resolve("./dist/" + cssName + ".json");
+        //     fs.writeFileSync(jsonFileName, JSON.stringify(json));
+        //   },
+        // })
+      ],
+      // modules: true,
+      extensions: ['.css', '.less'],
+      use: [['less', {
+        javascriptEnabled: true
+      }]],
+      // extract: 'bundle.css'
     }),
+    // less(),
 
     url(),
     svgr(),
@@ -36,7 +66,11 @@ export default {
       plugins: ['external-helpers']
     }),
     resolve({
-      jail: './src'
+      jail: './src',
+      jsnext: true,
+      main: true,
+      browser: true,
+      extensions: ['.js', '.jsx', '.ts', '.tsx', 'css', '.less']
     }),
     commonjs()
   ]
